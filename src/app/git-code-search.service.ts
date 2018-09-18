2283231d97e8a371771da19387e094be0edbb1f0
Matthew <mlking89@live.com>
2018-09-18 15:47:33 -0400

    bare bones

--- src/app/git-code-search.service.ts
+++ src/app/git-code-search.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { GitCodeSearch } from './git-code-search';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class GitCodeSearchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  codeSearch: Function = (query: string): Observable<GitCodeSearch> => {
+    return this.http.get<GitCodeSearch>('https://api.github.com/search/code?q=' + query)
+  }
 
 }
