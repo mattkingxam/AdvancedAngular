ab14262140cd353760db0217ece75cc024f0386f
Matthew <mlking89@live.com>
2018-09-18 14:43:59 -0400

    Update component - that was easy

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -8,29 +8,29 @@ import { AdvancedSearchModel } from '../advanced-search-model'
   templateUrl: './git-search.component.html',
   styleUrls: ['./git-search.component.css']
 })
-export class GitSearchComponent implements OnInit { 
+export class GitSearchComponent implements OnInit {
   searchResults: GitSearch;
   searchQuery: string;
   displayQuery: string;
   title: string;
-  constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router ) { }
+  constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute, private router: Router) { }
 
   model = new AdvancedSearchModel('', '', '', null, null, '');
   modelKeys = Object.keys(this.model);
 
   ngOnInit() {
-    this.route.paramMap.subscribe( (params: ParamMap) => {
+    this.route.paramMap.subscribe((params: ParamMap) => {
       this.searchQuery = params.get('query');
       this.displayQuery = params.get('query');
-      this.gitSearch();  
+      this.gitSearch();
     })
-    this.route.data.subscribe( (result) => {
+    this.route.data.subscribe((result) => {
       this.title = result.title
     });
   }
 
   gitSearch = () => {
-    this.GitSearchService.gitSearch(this.searchQuery).then( (response) => {
+    this.GitSearchService.gitSearch(this.searchQuery).subscribe((response) => {
       this.searchResults = response;
     }, (error) => {
       alert("Error: " + error.statusText)
@@ -39,19 +39,19 @@ export class GitSearchComponent implements OnInit {
 
   sendQuery = () => {
     this.searchResults = null;
-    let search : string = this.model.q;
-    let params : string = "";
-    this.modelKeys.forEach(  (elem) => {
-        if (elem === 'q') {
-            return false;
-        }
-        if (this.model[elem]) {
-            params += '+' + elem + ':' + this.model[elem];
-        }
+    let search: string = this.model.q;
+    let params: string = "";
+    this.modelKeys.forEach((elem) => {
+      if (elem === 'q') {
+        return false;
+      }
+      if (this.model[elem]) {
+        params += '+' + elem + ':' + this.model[elem];
+      }
     })
     this.searchQuery = search;
     if (params !== '') {
-        this.searchQuery = search + params;
+      this.searchQuery = search + params;
     }
     this.displayQuery = this.searchQuery;
     this.gitSearch();
